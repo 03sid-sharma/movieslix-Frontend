01896ac0eb26ef2c96f8d09cfa62cd2ddf51fdb4
03sid-sharma <03sid.sharma@gmail.com>
2020-12-25 21:00:04 +0530

    chore: navbar added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import "./App.css";
-import Movies from "./components/movies";
+import React from 'react';
 import { Redirect, Route, Switch } from "react-router-dom";
+import Movies from "./components/movies";
 import Customers from "./components/customers";
 import NotFound from "./components/notFound";
 import Rentals from "./components/rentals";
+import NavBar from "./components/navBar";
 
 function App() {
   return (
-    <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" exact to="/movies" />
-        <Redirect to="not-found"/>
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
