79bec306386b6a19daa25218121f866cabbc6461
03sid-sharma <03sid.sharma@gmail.com>
2020-12-25 20:12:55 +0530

    chore: added routing to website

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -17,7 +17,7 @@ const Pagination = ({ itemsCount, currentPage, pageSize, onPageChange }) => {
             className={page === currentPage ? "page-item active" : "page-item"}
           >
             <a
-              href="#"
+              href="/#"
               className="page-link"
               onClick={() => onPageChange(page)}
             >
---
+++ src/components/movieForm.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+const MovieForm = ({_id}) => {
+    return ( <h1>Movie Form {_id}</h1> );
+}
+ 
+export default MovieForm;
