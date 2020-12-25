a1c9ed89f19af9b9dc9ea67fb084c6b312fe29b5
03sid-sharma <03sid.sharma@gmail.com>
2020-12-25 21:13:15 +0530

    chore: movieForms save

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
-const MovieForm = ({_id}) => {
-    return ( <h1>Movie Form {_id}</h1> );
-}
- 
+import React from "react";
+const MovieForm = ({ match,history }) => {
+  return (
+    <div>
+      <h1>Movie Form {match.params.id}</h1>
+      <button className="btn btn-primary" onClick={()=>{ history.push("/movies")}}>Save</button>
+    </div>
+  );
+};
+
 export default MovieForm;
