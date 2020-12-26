a2f1e457359f344403a9be3e511f95235abbde5a
03sid-sharma <03sid.sharma@gmail.com>
2020-12-26 15:46:51 +0530

    chore: Login form created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
-import React from 'react';
+import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
 import NotFound from "./components/notFound";
 import Rentals from "./components/rentals";
 import NavBar from "./components/navBar";
-import MovieForm from './components/movieForm';
+import MovieForm from "./components/movieForm";
+import LoginForm from './components/loginForm';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
-        <Route path="/movies/:id" component={MovieForm}></Route>
+          <Route path="/login" component={LoginForm}></Route>
+          <Route path="/movies/:id" component={MovieForm}></Route>
           <Route path="/movies" component={Movies}></Route>
           <Route path="/customers" component={Customers}></Route>
           <Route path="/rentals" component={Rentals}></Route>
