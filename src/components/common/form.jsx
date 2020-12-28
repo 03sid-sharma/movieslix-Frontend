ac7da0ddbd8d423295ecc6c068190b5e4941920e
03sid-sharma <03sid.sharma@gmail.com>
2020-12-28 18:45:44 +0530

    chore: new movie component

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Input from "./input";
+import Select from "./select";
+
 class Form extends Component {
   state = {
     data: {},
@@ -62,6 +64,19 @@ class Form extends Component {
         name={name}
         value={data[name]}
         label={label}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+    return (
+      <Select
+        name={name}
+        value={data[name]}
+        label={label}
+        options={options}
         onChange={this.handleChange}
         error={errors[name]}
       />
